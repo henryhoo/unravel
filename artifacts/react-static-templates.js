4efc42f322a8c2e59a81e96cb095aae8e934df43
Jiaxi He <jxh@fb.com>
2020-10-04 15:32:24 -0400

    Generate nav bar based on markdown file tree

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -5,6 +5,7 @@
 export default {
   '__react_static_root__/src/pages/404.js': require('__react_static_root__/src/pages/404.js').default,
 '__react_static_root__/contents/journals/2020_09_28.md': require('__react_static_root__/contents/journals/2020_09_28.md').default,
+'__react_static_root__/contents/journals/README.md': require('__react_static_root__/contents/journals/README.md').default,
 '__react_static_root__/contents/static_react_website/html_css.md': require('__react_static_root__/contents/static_react_website/html_css.md').default
 }
 
---
+++ contents/journals/README.md
@@ -0,0 +1,5 @@
+---
+title: 1
+---
+
+# index page for subfolder
