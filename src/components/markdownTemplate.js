4d2460acf089518a52885bc22df3c74fe962df4c
Jiaxi He <jxh@fb.com>
2020-10-04 20:21:35 -0400

    Nav within md pages

--- src/components/markdownTemplate.js
+++ src/components/markdownTemplate.js
@@ -1,7 +1,35 @@
 import React from "react";
+import MarkdownNavbar from "markdown-navbar";
 
-export default ({ children }) => (
-    <main>
-      {children}
-    </main>
+const mdPages = require("react-static-plugin-md-pages");
+
+const article = `# Markdown-Navbar Demo
+
+## Chicken Chicken
+
+Chicken Chicken Chicken Chicken Chicken.
+
+* Chicken Chicken Chicken Chicken Chicken.
+* Chicken Chicken Chicken Chicken Chicken.
+* Chicken Chicken Chicken Chicken Chicken.
+
+### Chicken Chicken Chicken
+
+Chicken Chicken Chicken Chicken Chicken.
+
+#### Chicken Chicken Chicken Chicken
+
+Chicken Chicken Chicken Chicken Chicken Chicken.`;
+
+
+export default ({ children }) => {
+  const pageData = mdPages.useMarkdownPage()
+  console.log(pageData)
+  console.log(children)
+  return (
+    <div>
+      {/* {children} */}
+      <MarkdownNavbar source = {article}/>
+    </div>
   );
+};
